89296bbac5d8b730d53095af3f29029d018a131f
SachinFernandez <sachinfernandez.1401265@srec.ac.in>
2019-07-20 16:08:44 +0530

    adding nodemon and morgan dependencies, handling errors

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
-const userRoutes = require("./api/routes/users");
-const departmentRoutes = require("./api/routes/departments")
-
 const app = express()
+const morgan = require("morgan");
+
+const userRoutes = require("./api/routes/users");
+const departmentRoutes = require("./api/routes/departments");
+
+app.use(morgan('dev'));
 
 app.use('/users', userRoutes);
 app.use('/departments', departmentRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
