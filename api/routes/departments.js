47cb9124a6a434561b978b2fb4dd6d67c86b2e30
SachinFernandez <sachinfernandez.1401265@srec.ac.in>
2019-07-20 14:19:22 +0530

    Adding more routes to the API

---
+++ api/routes/departments.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'department were fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Department was created'
+    });
+});
+
+router.get('/:departmentId', (req, res, next) => {
+    const departmentId = req.params.departmentId
+    res.status(200).json({
+        message: 'Department details',
+        departmentId: departmentId
+    });
+});
+
+router.delete('/:departmentId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Department deleted',
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const userRoutes = require("./api/routes/users");
+const departmentRoutes = require("./api/routes/departments")
 
 const app = express()
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+app.use('/users', userRoutes);
+app.use('/departments', departmentRoutes);
 
 module.exports = app;
